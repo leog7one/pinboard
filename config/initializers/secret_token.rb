fdeb54b30004efc63aa168cbd3f4a239e09a532c
Leonardo Guzman <lguzman@me.com>
2016-04-10 20:17:14 +0000

    pinboard create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinboard::Application.config.secret_key_base = 'c9cb6b615460b5cab021b6d3da9ad0376a85dcd5f2646b45a27bec88378704408a50d3c20c901784112b11590fdd2784daf73192a292b598d55e4c72f7b18308'
