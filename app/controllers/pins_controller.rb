563c2423f20041dec8d1c3edebbf084add8578b0
Leonardo Guzman <lguzman@me.com>
2016-04-27 02:13:42 +0000

    added form works from new

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,33 @@
 class PinsController < ApplicationController
+	before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
 	def index
-end
+	end
+
+	def show
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+
+		if @pin.save
+			redirect_to @pin, notice: "Successfully created new Pin"
+		else
+			 render action 'new'
+		end
+	end
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
+
+	def set_pin
+      @pin = Pin.find(params[:id])
+    end
 end
